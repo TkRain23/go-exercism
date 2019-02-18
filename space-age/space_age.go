a633037814926e9153dd8b6025d902c164e05e08
TkRain23 <tk.rain09@gmail.com>
2019-02-17 16:37:02 -0800

    add space age solution

--- space-age/space_age.go
+++ space-age/space_age.go
@@ -1,6 +1,24 @@
 package space
 
-func findAge(age int, planet string) string {
-	myAge := age / planet
-	return ""
+func Age(seconds float64, planet string) float64 {
+	switch planet {
+	case "Earth":
+		return seconds / 31557600
+	case "Mercury":
+		return seconds / 7600387.75
+	case "Venus":
+		return seconds / 19413750.40
+	case "Mars":
+		return seconds / 59352813.92
+	case "Jupiter":
+		return seconds / 374347972.1
+	case "Saturn":
+		return seconds / 929273280.9
+	case "Uranus":
+		return seconds / 2649555255.46
+	case "Neptune":
+		return seconds / 5196859067.52
+	default:
+		return 0.00
+	}
 }
