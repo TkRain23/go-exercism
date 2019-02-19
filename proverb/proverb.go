e3b5d28b4140dcaa97540294d74c4fe0266a2cc9
TkRain23 <tk.rain09@gmail.com>
2019-02-19 02:15:04 -0800

    add proverb solution

--- proverb/proverb.go
+++ proverb/proverb.go
@@ -11,5 +11,20 @@ func Proverb(rhyme []string) []string {
 	// Then remove all the stock comments.
 	// They're here to help you get started but they only clutter a finished solution.
 	// If you leave them in, reviewers may protest!
-	return []string{}
+
+	if len(rhyme) == 0 {
+		return []string{}
+	}
+
+	var output []string
+
+	for i, val := range rhyme {
+		if i < len(rhyme)-1 {
+			output = append(output, "For want of a "+val+" the "+string(rhyme[i+1])+" was lost.")
+		} else {
+			output = append(output, "And all for the want of a "+string(rhyme[0])+".")
+		}
+	}
+
+	return output
 }
