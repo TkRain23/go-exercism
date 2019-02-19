82a1caf46b1d199c7e98b677abc072ef6cbe313d
TkRain23 <tk.rain09@gmail.com>
2019-02-19 01:23:07 -0800

    add triangle solution

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -5,24 +5,52 @@
 // https://golang.org/doc/effective_go.html#commentary
 package triangle
 
+import "math"
 
 // Notice KindFromSides() returns this type. Pick a suitable data type.
-type Kind
+type Kind int
 
 const (
-    // Pick values for the following identifiers used by the test program.
-    NaT // not a triangle
-    Equ // equilateral
-    Iso // isosceles
-    Sca // scalene
+	// Pick values for the following identifiers used by the test program.
+	Equ Kind = iota // equilateral
+	NaT             // not a triangle
+	Iso             // isosceles
+	Sca             // scalene
 )
 
 // KindFromSides should have a comment documenting it.
+const testVersion = 3
+
 func KindFromSides(a, b, c float64) Kind {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	var k Kind
-	return k
+	if !isTriangle(a, b, c) {
+		return NaT
+	} else if isEquilateral(a, b, c) {
+		return Equ
+	} else if isIsoceles(a, b, c) {
+		return Iso
+	} else if isScalene(a, b, c) {
+		return Sca
+	}
+	return NaT
+}
+
+func isTriangle(a, b, c float64) bool {
+	return isSide(a) && isSide(b) && isSide(c) &&
+		a+b >= c && a+c >= b && b+c >= a
+}
+
+func isSide(f float64) bool {
+	return f > 0 && !math.IsNaN(f) && !math.IsInf(f, 1)
+}
+
+func isEquilateral(a, b, c float64) bool {
+	return a == b && a == c && b == c
+}
+
+func isIsoceles(a, b, c float64) bool {
+	return a == b || b == c || a == c
+}
+
+func isScalene(a, b, c float64) bool {
+	return a != b && a != c && b != c
 }
